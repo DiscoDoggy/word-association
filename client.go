3d8d2094dba964f8b71d3020d973c9c20eb89ed5
DiscoDoggy <akaattarilim@gmail.com>
2025-11-03 14:01:16 -0800

    Added additional event tags to websocket client senders

--- client.go
+++ client.go
@@ -10,6 +10,9 @@ type Client struct {
 	id			uuid.UUID	
 	connection *websocket.Conn
 	manager    *Manager
+
+	//to avoid concurrent writes to the websocket
+	egress chan []byte
 }
 
 type ClientList map[*Client]bool
@@ -19,6 +22,7 @@ func NewClient(conn *websocket.Conn, manager *Manager) *Client {
 		id: uuid.New(),
 		connection: conn,
 		manager: manager,
+		egress: make(chan []byte),
 	}
 }
 
@@ -36,6 +40,35 @@ func (c *Client) readMessages() {
 		}
 		log.Println("Message type: ", messageType)
 		log.Println("Payload: ", string(payload))
+
+		//for testing
+		for wsclient := range c.manager.clients {
+			wsclient.egress <- payload
+		}
+	}
+}
+
+func (c *Client) writeMessages() {
+	defer func() {
+		c.manager.removeClient(c)
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.egress:
+			if !ok {
+				if err := c.connection.WriteMessage(websocket.CloseMessage, nil); err != nil {
+					log.Println("connection closed: ", err)
+				}
+
+				return
+			}
+
+			if err := c.connection.WriteMessage(websocket.TextMessage, message); err != nil {
+				log.Println(err)
+			}
+			log.Println("sent message")
+		}
 	}
 }
 
