3bd821635c8fa18728e5e9d3171228c161f40f3f
DiscoDoggy <akaattarilim@gmail.com>
2025-12-05 20:34:54 -0800

    Centered card on index page correctly

--- templates/base_templ.go
+++ templates/base_templ.go
@@ -64,7 +64,7 @@ func Base(title string, bodyStyles string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "\"><div id=\"page-content\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "\"><div id=\"page-content\" class=\"flex flex-col h-screen\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
